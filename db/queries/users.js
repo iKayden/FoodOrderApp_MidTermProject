8230c6fa0e43cfb6f6dcc17e21f930ea7d386408
ikayden <ikyden23@gmail.com>
2022-10-22 01:37:08 -0700

    Delete widgets (not needed) and change scope of query strings

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,40 +1,41 @@
 const db = require('../connection');
 
 const getAllCustomers = () => { //Used by Admin
-  return db.query('SELECT * FROM customers;')
+  const allCustomersQuery = 'SELECT * FROM customers;';
+  return db.query(allCustomersQuery)
     .then(data => {
       return data.rows;
     });
 };
 
-const cartQuery = 'SELECT * FROM cart_items JOIN products ON products.id = cart_items.product_id JOIN orders ON orders.id = cart_items.order_id JOIN order_time ON order_time.id = orders.order_time_id;';
 
 const getAllCartItems = () => {
+  const cartQuery = 'SELECT * FROM cart_items JOIN products ON products.id = cart_items.product_id JOIN orders ON orders.id = cart_items.order_id JOIN order_time ON order_time.id = orders.order_time_id;';
   return db.query(cartQuery)
     .then(data => {
       return data.rows;
     });
 };
 
-const oneCartItemQuery = 'SELECT * FROM cart_items JOIN products ON products.id = cart_items.product_id JOIN orders ON orders.id = cart_items.order_id WHERE cart_items.product_id = $1;';
 
 const getOneCartItem = (itemId) => {
+  const oneCartItemQuery = 'SELECT * FROM cart_items JOIN products ON products.id = cart_items.product_id JOIN orders ON orders.id = cart_items.order_id WHERE cart_items.product_id = $1;';
   return db.query(oneCartItemQuery, [itemId])
     .then(data => {
       return data.rows[0];
     });
 };
 
-const paymentQuery = 'SELECT * FROM payments WHERE order_id = $1;';
 const paymentDetails = (orderId) => {
+  const paymentQuery = 'SELECT * FROM payments WHERE order_id = $1;';
   return db.query(paymentQuery, [orderId])
     .then(data => {
       return data.rows[0];
     });
 };
 
-const getOrdersQuery = 'SELECT * FROM orders;';
 const getOrderDetails = () => {
+  const getOrdersQuery = 'SELECT * FROM orders;';
   return db.query(getOrdersQuery)
     .then(data => {
       return data.rows[0];
