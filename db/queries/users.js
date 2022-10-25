d887eb49d6fc58e85e5ffe2366dea550d516642e
lyjeileen <lyjeileen@gmail.com>
2022-10-25 16:55:48 -0700

    Use one single query
    See: https://stackoverflow.com/questions/5803472/sql-where-id-in-id1-id2-idn

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,31 +1,29 @@
-const db = require('../connection');
+const db = require("../connection");
 
-const getAllCustomers = () => { //Used by Admin
-  const allCustomersQuery = 'SELECT * FROM customers;';
-  return db.query(allCustomersQuery)
-    .then(data => {
-      return data.rows;
-    });
+const getAllCustomers = () => {
+  //Used by Admin
+  const allCustomersQuery = "SELECT * FROM customers;";
+  return db.query(allCustomersQuery).then((data) => {
+    return data.rows;
+  });
 };
 
 const getAllProducts = () => {
   const productsQuery = `SELECT * FROM products;`;
-  return db.query(productsQuery)
-    .then(data => {
-      return data.rows;
-    });
+  return db.query(productsQuery).then((data) => {
+    return data.rows;
+  });
 };
 
 const getOneProduct = (productId) => {
   const oneProductQuery = `
-  SELECT * FROM products WHERE id = $1;`;
-  return db.query(oneProductQuery, [productId])
-    .then(data => {
-      // console.log('data.rows--->', data.rows[0])
-      return data.rows[0];
-    });
+  SELECT * FROM products WHERE id = $1;
+  `;
+  return db.query(oneProductQuery, [productId]).then((data) => {
+    console.log("data.rows--->", data.rows[0]);
+    return data.rows[0];
+  });
 };
-
 
 const getAllInfo = () => {
   const cartQuery = `
@@ -35,64 +33,72 @@ const getAllInfo = () => {
   JOIN order_time ON order_time.id = orders.order_time_id;
   `;
 
-  return db.query(cartQuery)
-    .then(data => {
-      return data.rows;
-    });
+  return db.query(cartQuery).then((data) => {
+    return data.rows;
+  });
 };
 
-
 const getOneCartItem = (itemId) => {
-  const oneCartItemQuery =
-  `SELECT * FROM cart_items
+  const oneCartItemQuery = `SELECT * FROM cart_items
   JOIN products ON products.id = cart_items.product_id
   JOIN orders ON orders.id = cart_items.order_id
   WHERE cart_items.product_id = $1;
   `;
 
-  return db.query(oneCartItemQuery, [itemId])
-    .then(data => {
-      return data.rows[0];
-    });
+  return db.query(oneCartItemQuery, [itemId]).then((data) => {
+    return data.rows[0];
+  });
 };
 
 const paymentDetails = (orderId) => {
-  const paymentQuery = 'SELECT * FROM payments WHERE order_id = $1;';
-  return db.query(paymentQuery, [orderId])
-    .then(data => {
-      return data.rows[0];
-    });
+  const paymentQuery = "SELECT * FROM payments WHERE order_id = $1;";
+  return db.query(paymentQuery, [orderId]).then((data) => {
+    return data.rows[0];
+  });
 };
 
 const getOrderDetails = () => {
-  const getOrdersQuery = 'SELECT * FROM orders;';
-  return db.query(getOrdersQuery)
-    .then(data => {
-      return data.rows[0];
-    });
+  const getOrdersQuery = "SELECT * FROM orders;";
+  return db.query(getOrdersQuery).then((data) => {
+    return data.rows[0];
+  });
 };
 
-const addOrder = function(order) {
-  // console.log("ORDER DATA FROM ADDORDER FUNCTION IN THE DATA AND CUSTOMER",order.customer);
-  // console.log("phone", order.customer.phone);
+const addOrder = function (order) {
+  console.log(
+    "ORDER DATA FROM ADDORDER FUNCTION IN THE DATA AND CUSTOMER",
+    order.customer
+  );
+  console.log("phone", order.customer.phone);
   return db
     .query(
       `INSERT INTO orders (phone, total_cost)
-      VALUES ($1 ,$2) RETURNING *`, [order.customer.phone, order.total_cost]
+      VALUES ($1 ,$2) RETURNING *`,
+      [order.customer.phone, order.total_cost]
     )
-    .then(data=> {
+    .then((data) => {
       return data.rows[0];
-    })
+    });
 };
 
-const getPriceById = (id) =>{
-  const productsQuery = `SELECT price FROM products WHERE id = $1`;
-  return new Promise((resolve, reject) => {
-  db.query(productsQuery, [id])
-    .then(data => {
-      resolve(data.rows[0].price)
+const getProductsByIds = function (ids) {
+  return db
+    .query(`SELECT id, name, price FROM products WHERE id IN (${ids})`)
+    .then((data) => {
+      const productsInfo = {};
+      data.rows.forEach((row) => (productsInfo[row.id] = row));
+      return productsInfo;
     });
-  })
-}
+};
 
-module.exports = { getPriceById, getAllCustomers, getAllInfo, getOneCartItem, paymentDetails, getOrderDetails , getAllProducts, getOneProduct, addOrder };
+module.exports = {
+  getAllCustomers,
+  getAllInfo,
+  getOneCartItem,
+  paymentDetails,
+  getOrderDetails,
+  getAllProducts,
+  getOneProduct,
+  addOrder,
+  getProductsByIds,
+};
--- routes/orders-api.js
+++ routes/orders-api.js
@@ -1,64 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userQueries = require('../db/queries/users');
-const twilio = require('../public/scripts/users');
+const userQueries = require("../db/queries/users");
+const twilio = require("../public/scripts/users");
 
 module.exports = router;
 
-router.get('/:id', (req, res) => { // ask mentor about :id
-  userQueries.getOrderDetails(req.params.id) //Changed the function a bit
-    .then(orderDetails => {
+router.get("/:id", (req, res) => {
+  // ask mentor about :id
+  userQueries
+    .getOrderDetails(req.params.id) //Changed the function a bit
+    .then((orderDetails) => {
       res.json({ orderDetails });
     })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
     });
 });
 
 // POST request for orders
-router.post('/', async (req, res) => {
-
+router.post("/", (req, res) => {
   const body = req.body;
-  Promise.all(body.cart_items.map(row => {
+  const ids = body.cart_items.map((item) => item.product_id).join(",");
+  userQueries.getProductsByIds(ids).then((data) => {
+    let totalCost = 0;
+    body.cart_items.forEach((item) => {
+      const cost = item.quantity * data[item.product_id].price;
+      totalCost += cost;
+    });
+    totalCost *= 1.05;
+    totalCost = Math.round(totalCost * 100) / 100;
 
-    return userQueries.getPriceById(row.product_id)
-      .then((table) => {
-        row.price = table;
-        return row;
-      });
-  })).then(response => {
-    let totalPrice = 0;
-    for (let item of response) {
-      totalPrice += (item.quantity * item.price);
-      // totalPrice = totalPrice + tempPrice;
-    }
-    let tax = totalPrice * (0.05);
-    let totalAmount = totalPrice + tax;
-    console.log("Total Amount ", totalAmount);
-
-    //// TWILLIO AND DATABASE STUFF
-    // return res.json({});
-    userQueries.addOrder(body)
+    userQueries
+      .addOrder(body)
       .then((data) => {
         // msg to the customer
-        twilio.sendText(`Hey, we have your order! Order ID is => ${data.id}, Your total cost is ${data.total_cost}`);
-        return { data };
-      }).then((data) => {
-        // msg to owner
-        twilio.sendText(`Hey, we have a new order! Order ID is => ${data.id}, The total cost is ${data.total_cost}`);
-        return { data };
-      }).then((data) => {
-        // res.json( {data} );
-        res.json({ message: 'Success!' });
-      }).catch(e => {
+        twilio.sendText(
+          `Hey, we have your order! Order ID is => ${
+            data.id
+          }, Your total cost is $${data.total_cost / 100}.`
+        );
+        // msg to the owner
+        twilio.sendText(
+          `Hey, we have a new order! Order ID is => ${
+            data.id
+          }, The total cost is $${data.total_cost / 100}.`
+        );
+        res.json({ message: "Success!" });
+      })
+      .catch((e) => {
         console.log(e);
         res.json(e);
       });
-    // twillio customer gets 3 msg "Recieved your order", "It will take x mins", "Order is ready"
-    // need extra post routes for messages
-    // one more page with
   });
-}); //Router.post ENDS here.
+});
 
+// twillio customer gets 3 msg "Recieved your order", "It will take x mins", "Order is ready"
+// need extra post routes for messages
+// one more page with
