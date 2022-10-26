dd19515e4665984734c0a81369dcf509daa9fe91
ikayden <ikyden23@gmail.com>
2022-10-26 00:32:25 -0700

    Add admin and user different pages

--- routes/products-api.js
+++ routes/products-api.js
@@ -1,20 +1,35 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const userQueries = require('../db/queries/users');
 
 
 router.get('/', (req, res) => { //gives all the info front end needs
-  res.cookie('user_id', 'customer') // cookie set-up 
-  userQueries.getAllProducts()
-    .then(info => {
-      console.log("Cookie data (name) ====>", req.cookies.fake_cookie);
-      res.json({ info });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
+  // res.cookie('user_id', 'customer') // cookie set-up 
+  console.log("REQ QUERY", req.query);
+  if (req.cookies.user_id === "admin") {
+    userQueries.getOrders()
+      .then(orders => {
+        const templateVars = { orders , type: "orders"}
+        res.json(templateVars);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+
+  } else {
+    userQueries.getAllProducts()
+      .then(info => {
+        console.log("Cookie data (name) ====>", req.cookies.user_id);
+        res.json({ info, type: "product"});
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  }
 });
 
 router.get('/:id', (req, res) => {
