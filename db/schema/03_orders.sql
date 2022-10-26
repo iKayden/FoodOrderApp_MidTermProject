c986dfc6d6010b3e34c4c6b204996c023ee1f264
lyjeileen <lyjeileen@gmail.com>
2022-10-26 11:30:01 -0700

    Change database structure for orders table

--- db/schema/03_orders.sql
+++ db/schema/03_orders.sql
@@ -1,10 +1,16 @@
--- Drop and recreate orders table
+-- Drop and recreate orders table AND order status type
 DROP TABLE IF EXISTS orders CASCADE;
+DROP TYPE IF EXISTS ORDERSTATUS CASCADE;
 
+--(Enum type) Add ORDERSTATUS type to limit the input. 0: unconfirmed, 1: confirmed, 2: finished
+CREATE TYPE ORDERSTATUS AS ENUM ('UNCONFIRMED','CONFIRMED','FINISHED');
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   phone VARCHAR(255) NOT NULL,
-  total_cost INTEGER NOT NULL
+  total_cost INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  ready_at TIMESTAMP,
+  status ORDERSTATUS DEFAULT 'UNCONFIRMED'
 );
   -- order_date DATE NOT NULL,
   -- order_time_id INTEGER REFERENCES order_time(id)
